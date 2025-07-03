57399f85bfca4615934b637bfd23eaf0d9b9cf25
Goutham-puram <puram.goutham@gmail.com>
2025-07-03 12:11:04 -0700

    Move Babel config to web

--- web/babel.config.js
+++ web/babel.config.js
@@ -1,1 +1,6 @@
-module.exports = require('../babel.config');
+const path = require('path');
+const alias = { shared: path.join(__dirname, '../shared') };
+module.exports = {
+  presets: [['next/babel'], '@babel/preset-flow'],
+  plugins: [['module-resolver', { alias }]]
+};
